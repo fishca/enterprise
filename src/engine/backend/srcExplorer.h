8b2776bca8a6892463b12ef735d518a2c294d64d
Max <Max@nouverbe>
2026-01-08 20:37:15 +0200

    Updated CSourceExplorer class

--- src/engine/backend/srcExplorer.h
+++ src/engine/backend/srcExplorer.h
@@ -2,107 +2,125 @@
 #define __SRC_EXPLORER_H__
 
 class BACKEND_API CSourceExplorer {
-	struct CSourceData {
+
+	struct CSourceInfo {
+
 		wxString m_srcName;
 		wxString m_srcSynonym;
-		IMetaObject* m_metaObject = nullptr;
-		meta_identifier_t m_meta_id = wxNOT_FOUND;
-		CTypeDescription m_typeDesc;
+
+		meta_identifier_t m_mid;
+
 		bool m_enabled = true;
 		bool m_visible = true;
 		bool m_tableSection = false;
 		bool m_select = true;
+
+		CTypeDescription m_typeDesc;
+
+		const IMetaObject* m_metaObject = nullptr;
 	};
+
 private:
 
 	CSourceExplorer() {}
 
-	CSourceExplorer(IMetaObjectCompositeData* refData, const class_identifier_t& clsid) {
-		m_srcData = { wxT("ref"), _("Ref"), refData, refData->GetMetaID(), { clsid }, true, false };
+	CSourceExplorer(const IMetaObjectCompositeData* object, const class_identifier_t& cid) {
 
-		for (const auto object : refData->GetGenericAttributeArrayObject()) {
-			CSourceExplorer::AppendSource(object);
-		}
+		m_sourceInfo = {
+			wxT("ref"), _("Ref"), object->GetMetaID(), true, false, false, true, { cid }, object
+		};
+
+		for (const auto child : object->GetGenericAttributeArrayObject()) CSourceExplorer::AppendSource(child);
 	}
 
-	CSourceExplorer(IMetaObjectAttribute* attribute, bool enabled = true, bool visible = true) {
-		m_srcData = { attribute->GetName(), attribute->GetSynonym(), attribute, attribute->GetMetaID(), attribute->GetTypeDesc(), enabled, visible, false };
+	CSourceExplorer(const IMetaObjectAttribute* object, bool enabled = true, bool visible = true) {
+
+		m_sourceInfo = {
+			object->GetName(), object->GetSynonym(), object->GetMetaID(), enabled, visible, false, true, object->GetTypeDesc(), object
+		};
 	}
 
-	CSourceExplorer(CMetaObjectTableData* tableSection) {
-		m_srcData = { tableSection->GetName(), tableSection->GetSynonym(), tableSection, tableSection->GetMetaID(), tableSection->GetTypeDesc(), true, true, true };
-		for (const auto object : tableSection->GetGenericAttributeArrayObject()) {
-			CSourceExplorer::AppendSource(object);
-		}
+	CSourceExplorer(const CMetaObjectTableData* object) {
+
+		m_sourceInfo = {
+			object->GetName(), object->GetSynonym(), object->GetMetaID(), true, true, true, true,  object->GetTypeDesc(), object
+		};
+
+		for (const auto child : object->GetGenericAttributeArrayObject()) CSourceExplorer::AppendSource(child);
 	}
 
 public:
 
-	CSourceExplorer(IMetaObject* refData, const class_identifier_t& clsid, bool tableSection, bool select = false) {
-		m_srcData = { wxT("ref"), _("Ref"), refData, refData->GetMetaID(), clsid, true, true, tableSection, select };
+	CSourceExplorer(const IMetaObject* object, const class_identifier_t& cid, bool tableSection, bool select = false) {
+
+		m_sourceInfo = {
+			wxT("ref"), _("Ref"), object->GetMetaID(), true, true, tableSection, select, cid, object
+		};
 	}
 
 	// this object 
-	CSourceExplorer(IMetaObjectGenericData* refData, const class_identifier_t& clsid, bool tableSection, bool select = false) {
-		if (refData->IsDeleted())
+	CSourceExplorer(const IMetaObjectGenericData* object, const class_identifier_t& cid, bool tableSection, bool select = false) {
+
+		if (object->IsDeleted())
 			return;
-		m_srcData = { refData->GetName(), refData->GetSynonym(), refData, refData->GetMetaID(), clsid,  true, true, tableSection, select };
+
+		m_sourceInfo = {
+			object->GetName(), object->GetSynonym(), object->GetMetaID(), true, true, tableSection, select, cid, object
+		};
 	}
 
-	wxString GetSourceName() const { return m_srcData.m_srcName; }
-	wxString GetSourceSynonym() const { return m_srcData.m_srcSynonym; }
-	meta_identifier_t GetSourceId() const { return m_srcData.m_meta_id; }
+	wxString GetSourceName() const { return m_sourceInfo.m_srcName; }
+	wxString GetSourceSynonym() const { return m_sourceInfo.m_srcSynonym; }
 
-	bool IsEnabled() const { return m_srcData.m_enabled; }
-	bool IsVisible() const { return m_srcData.m_visible; }
-	bool IsTableSection() const { return m_srcData.m_tableSection; }
-	bool IsSelect() const { return m_srcData.m_select && IsAllowed(); }
+	meta_identifier_t GetSourceId() const { return m_sourceInfo.m_mid; }
+
+	bool IsEnabled() const { return m_sourceInfo.m_enabled; }
+	bool IsVisible() const { return m_sourceInfo.m_visible; }
+	bool IsTableSection() const { return m_sourceInfo.m_tableSection; }
+	bool IsSelect() const { return m_sourceInfo.m_select && IsAllowed(); }
 	bool IsAllowed() const { return GetMetaObject()->IsAllowed(); }
 
-	IMetaObject* GetMetaObject() const { return m_srcData.m_metaObject; }
+	const IMetaObject* GetMetaObject() const { return m_sourceInfo.m_metaObject; }
+	const std::vector<class_identifier_t>& GetClsidList() const { return m_sourceInfo.m_typeDesc.GetClsidList(); }
 
-	const std::vector<class_identifier_t>& GetClsidList() const { return m_srcData.m_typeDesc.GetClsidList(); }
+	bool ContainType(const eValueTypes& valType) const { return m_sourceInfo.m_typeDesc.ContainType(valType); }
+	bool ContainType(const class_identifier_t& cid) const { return m_sourceInfo.m_typeDesc.ContainType(cid); }
 
-	bool ContainType(const eValueTypes& valType) const { return m_srcData.m_typeDesc.ContainType(valType); }
-	bool ContainType(const class_identifier_t& clsid) const { return m_srcData.m_typeDesc.ContainType(clsid); }
-
-	void AppendSource(IMetaObjectGenericData* refData, const class_identifier_t& clsid) {
+	void AppendSource(IMetaObjectGenericData* refData, const class_identifier_t& cid) {
 		if (refData->IsDeleted())
 			return;
-		m_listSrcData.emplace_back(
-			CSourceExplorer{ refData, clsid }
-		);
+		m_arraySource.emplace_back(CSourceExplorer{ refData, cid });
 	}
 
 	void AppendSource(IMetaObjectAttribute* attribute, bool enabled = true, bool visible = true) {
 		if (attribute->IsDeleted())
 			return;
-		m_listSrcData.emplace_back(
-			CSourceExplorer{ attribute, enabled , visible }
-		);
+		m_arraySource.emplace_back(CSourceExplorer{ attribute, enabled , visible });
 	}
 
 	void AppendSource(CMetaObjectTableData* tableSection) {
+		
 		if (tableSection->IsDeleted())
 			return;
-		m_listSrcData.emplace_back(
-			CSourceExplorer{ tableSection }
-		);
+		
+		m_arraySource.emplace_back(CSourceExplorer{ tableSection });
 	}
 
 	CSourceExplorer GetHelper(unsigned int idx) const {
-		if (m_listSrcData.size() < idx) {
+		
+		if (m_arraySource.size() < idx)
 			return CSourceExplorer();
-		}
-		return m_listSrcData[idx];
+		
+		return m_arraySource[idx];
 	}
 
-	unsigned int GetHelperCount() const { return m_listSrcData.size(); }
+	unsigned int GetHelperCount() const { return m_arraySource.size(); }
 
 protected:
 
-	std::vector<CSourceExplorer> m_listSrcData;
-	CSourceData m_srcData;
+	CSourceInfo m_sourceInfo;
+
+	std::vector<CSourceExplorer> m_arraySource;
 };
 
 #include "backend/srcObject.h"
